0c1b61dbf94712b105be828655e10847cc0a92aa
Ilham F Saputra <ilhamfathys@gmail.com>
2016-08-02 19:15:21 +0700

    Add GameController Script
    Wait for akang lutfi to continue

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void killEnemies(string color){
+		Collider2D[] enemies = Physics2D.OverlapBoxAll(transform.position, new Vector2(15,10), 1 << LayerMask.NameToLayer("Enemy"));
+		foreach (Collider2D enemy in enemies) {
+			enemy.GetComponent<Blabla> ();
+			if (color == enemy.getColorName ()) {
+				enemy.destroyEnemy ();
+			}
+		}
+	}
+
+}
