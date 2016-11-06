e180be7a27423431f22f9e1a66f79a14939a527c
Muhammad Luthfi <luthfi.muhammad825@gmail.com>
2016-11-06 12:36:49 +0700

    increase special item chance

---
+++ Assets/SpecialSpawnerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialSpawnerScript : MonoBehaviour {
+	public GameObject rainbow;
+	public GameObject time;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (spawn(15.0f));
+	}
+
+	IEnumerator spawn(float wait) {
+		yield return new WaitForSeconds (wait);
+		Instantiate (rainbow, transform.position, Quaternion.identity);
+		yield return new WaitForSeconds (wait + 5.0f);
+		Instantiate (time, transform.position, Quaternion.identity);
+		yield break;
+	}
+}
